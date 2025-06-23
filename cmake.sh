2094ce918f16a73f4b5900050fc6c35691ff07df
Jaymes <urmomrickroll1@gmail.com>
2025-06-23 13:58:50 -0400

    updated cmake.sh to convert elf to 3dsx

--- cmake.sh
+++ cmake.sh
@@ -2,3 +2,4 @@ rm -fr ~/isle-portable-3ds/build
 cmake -S. -Bbuild -DCMAKE_TOOLCHAIN_FILE="$DEVKITPRO/cmake/3DS.cmake" -DCMAKE_BUILD_TYPE=Release -DISLE_BUILD_CONFIG=OFF
 cd build 
 make
+3dsxtool isle.elf isle.3dsx
